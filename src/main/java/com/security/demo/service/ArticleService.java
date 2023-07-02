6eaf89ce41ad6d0b87f32029aa5de671cf22c330
chris1797 <jaehun6004@gmail.com>
2023-07-02 23:29:16 +0900

    Service interface remove

--- src/main/java/com/security/demo/service/ArticleService.java
+++ src/main/java/com/security/demo/service/ArticleService.java
@@ -11,32 +11,29 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ArticleService implements ArticleService {
+public class ArticleService {
 
     private final ArticleRepository articleRepository;
     private final ArticleQueryRepository articleQueryRepository;
 
 
-    @Override
     @Transactional
     public void deleteArticle(Long article_idx) {
         articleRepository.deleteById(article_idx);
     }
 
-    @Override
-    @Transactional
+
+    @Transactional(readOnly = true)
     public Article getArticleDetail(Long article_idx) {
         Article article = articleRepository.findById(article_idx)
                 .orElseThrow(() -> new NullPointerException("This article does not exist."));
         return article;
     }
 
-    @Override
     public List<Article> getAllArticle() {
         return articleRepository.findAll();
     }
 
-    @Override
     @Transactional
     public Long writeArticle(Article article) {
         return articleRepository.save(article).getArticle_idx();
--- src/test/java/com/security/demo/memberTest/MemberTest.java
+++ src/test/java/com/security/demo/memberTest/MemberTest.java
@@ -1,5 +1,6 @@
 package com.security.demo.memberTest;
 
+import com.security.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
