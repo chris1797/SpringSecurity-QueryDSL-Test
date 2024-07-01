28f769ba19f3d13b93a7836cd25054a564c12cc7
chris1797 <jaehun6004@gmail.com>
2024-07-01 23:30:00 +0900

    update

--- src/main/java/com/security/demo/common/config/BeanConfig.java
+++ src/main/java/com/security/demo/common/config/BeanConfig.java
@@ -1,5 +1,7 @@
 package com.security.demo.common.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
